b87b04d5c20e963a69e3121c0e6553984af2e9e8
Javier <jmartinez@identicum.com>
2020-04-19 14:58:36 -0300

    agregado de trigger, pruebas y actualizacion de tablas

---
+++ Test/03.Pruebas.sql
@@ -0,0 +1,12 @@
+INSERT INTO pruebaGit
+VALUES(1, 'Juan', 30)
+
+INSERT INTO pruebaGit
+VALUES(2, 'Javier', 27)
+
+UPDATE pruebaGit 
+SET edad = 28
+WHERE nombre = 'Juan'
+
+DELETE FROM pruebaGit 
+WHERE nombre = 'Juan'
